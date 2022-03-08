69cb0b4193c9020ae59b974927e4149d6d5b687d
vikram <vikramvikram741741@gmail.com>
2022-03-08 19:43:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'vhatsapp-bot';
+	title = 'VhatsApp';
+	locked = false;
+	password = "1234";
+	inputText = "";
+	wrongPassword = false;
+
+	unlock() {
+		if (this.password == this.inputText) {
+			this.locked = false;
+			return;
+		}
+		this.wrongPassword = true;
+	}
 }
---
+++ src/app/services/va.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Injectable()
+export class VaService {
+
+    private baseUrl = 'http://localhost:3000/api'
+    // private baseUrl = ''
+
+    constructor(private apiService: ApiService) { }
+
+    createSession() {
+        return this.apiService.get(`${this.baseUrl}/createSession`);
+    }
+
+    sendMessages(data: any) {
+        console.log(data)
+        return this.apiService.post(`${this.baseUrl}/sendMessages`, data);
+    }
+}

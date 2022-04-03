9711f8a6a4796618d542471e55dd4c9ca2d9277d
vikram <vikramvikram741741@gmail.com>
2022-04-03 09:41:25 +0530

    3000

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', (req,res)=>{
 
 
 /** Get port from environment and store in Express. */
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '3000';
 // app.listen(port, () => {
 //     console.log(`Server listening on the port::${port}`);
 // });
